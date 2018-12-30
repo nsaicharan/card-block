a534da780b7a5ef27f640fa249d68be415656692
Sai <saicharan13@gmail.com>
2018-12-31 00:57:08 +0530

    Implement layout of the card

--- src/block/block.js
+++ src/block/block.js
@@ -1,59 +1,116 @@
-/**
- * BLOCK: card-block
- *
- * Registering a basic block with Gutenberg.
- * Simple block, renders and saves the same content without any interactivity.
- */
-
-//  Import CSS.
+const { registerBlockType } = wp.blocks;
+const { RichText, MediaUpload, PlainText } = wp.editor;
+const { Button } = wp.components;
 import "./style.scss";
 import "./editor.scss";
 
-const { __ } = wp.i18n; // Import __() from wp.i18n
-const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+registerBlockType("card-block/main", {
+	title: "Card Block",
+	icon: "grid-view", // https://developer.wordpress.org/resource/dashicons/.
+	category: "common",
 
-/**
- * Register: aa Gutenberg Block.
- *
- * Registers a new block provided a unique name and an object defining its
- * behavior. Once registered, the block is made editor as an option to any
- * editor interface where blocks are implemented.
- *
- * @link https://wordpress.org/gutenberg/handbook/block-api/
- * @param  {string}   name     Block name.
- * @param  {Object}   settings Block settings.
- * @return {?WPBlock}          The block, if it has been successfully
- *                             registered; otherwise `undefined`.
- */
-registerBlockType("cgb/card-block", {
-	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __("Card Block"), // Block title.
-	icon: "shield", // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-	category: "common", // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
-	keywords: [__("card"), __("card block")],
-
-	/**
-	 * The edit function describes the structure of your block in the context of the editor.
-	 * This represents what the editor will render when the block is used.
-	 *
-	 * The "edit" property must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
-	edit: function(props) {
-		// Creates a <p class='wp-block-cgb-block-card-block'></p>.
-		return <div>Card</div>;
+	attributes: {
+		imgURL: {
+			type: "string",
+			source: "attribute",
+			attribute: "src",
+			selector: "img"
+		},
+		imgID: {
+			type: "number"
+		},
+		imgAlt: {
+			type: "string",
+			source: "attribute",
+			attribute: "alt",
+			selector: "img"
+		},
+		title: {
+			type: "string",
+			source: "text",
+			selector: ".g-card__title"
+		},
+		body: {
+			type: "array",
+			source: "children",
+			selector: ".g-card__body"
+		}
 	},
 
-	/**
-	 * The save function defines the way in which the different attributes should be combined
-	 * into the final markup, which is then serialized by Gutenberg into post_content.
-	 *
-	 * The "save" property must be specified and must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
-	save: function(props) {
-		return <div>Card</div>;
+	edit: function({ attributes, setAttributes, isSelected }) {
+		const handleSelect = img => {
+			setAttributes({
+				imgURL: img.url,
+				imgAlt: img.alt,
+				imgID: img.id
+			});
+		};
+
+		const removeImage = () => {
+			setAttributes({ imgURL: null });
+		};
+
+		const getImage = open => {
+			if (attributes.imgURL) {
+				return (
+					<img
+						className="g-card__img"
+						src={attributes.imgURL}
+						alt={attributes.imgAlt}
+					/>
+				);
+			} else {
+				return (
+					<div className="g-card__button-wrapper">
+						<Button className="button" onClick={open}>
+							Select Image
+						</Button>
+					</div>
+				);
+			}
+		};
+
+		return (
+			<div className="g-card">
+				<MediaUpload
+					onSelect={handleSelect}
+					value={attributes.imgID}
+					render={({ open }) => getImage(open)}
+				/>
+
+				<div className="g-card__content-wrapper">
+					<PlainText
+						className="g-card__title"
+						placeholder="Enter title..."
+						value={attributes.title}
+						onChange={title => setAttributes({ title })}
+					/>
+
+					<RichText
+						className="g-card__body"
+						placeholder="Body text here..."
+						value={attributes.body}
+						multiline="p"
+						onChange={body => setAttributes({ body })}
+					/>
+				</div>
+			</div>
+		);
+	},
+
+	save: function({ attributes }) {
+		return (
+			<div className="g-card">
+				<img
+					src={attributes.imgURL}
+					alt={attributes.imgAlt}
+					className="g-card__img"
+				/>
+				<div className="g-card__content-wrapper">
+					<h2 className="g-card__title">{attributes.title}</h2>
+					<div className="g-card__body">{attributes.body}</div>
+				</div>
+			</div>
+		);
 	}
 });
